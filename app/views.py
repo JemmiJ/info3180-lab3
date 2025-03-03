210c0607fbba8024e32dc751b6baccfc948cda69
JemmiJ <jemoiae@gmail.com>
2025-03-02 19:22:56 -0500

    Added contact form

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from app import app
+from app import app, mail
 from flask import render_template, request, redirect, url_for, flash
-
+from flask_mail import Message
+from .forms import ContactForm
 
 ###
 # Routing for your application.
@@ -17,6 +18,30 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        subject = form.subject.data
+        message = form.message.data
+    
+        msg = Message(subject = subject,
+                  sender = (name, email),
+                  recipients = ["to@example.com"])
+        msg.body = message
+
+        try:
+            mail.send(msg)
+            flash("Your Email was sent successfully!", "success")
+        except Exception as e:
+            flash(f"Error sending email: {str(e)}", "danger")
+        return redirect('/success')
+    return render_template('contact.html', form = form)
+    
 
 ###
 # The functions below should be applicable to all Flask apps.
